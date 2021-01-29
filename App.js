487846b5fdf35ea38fad8f947b99d0b0377de819
dasisvas1 <krva18ab@student.cbs.dk>
2021-01-29 13:20:09 +0100

    Camera med tracking mundbind virker. + camera går ikke i sort når der skiftes mellem tabnavigator + tabnavigator er en test screen

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Camera from './components/screens/Camera';
+//import React from 'react';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import Camera_v2 from './components/screens/Camera_v2';
+import Test from "./components/screens/Test";
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import {createAppContainer} from "react-navigation";
+import {Entypo} from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <Camera_v2/>
-  );
+
+const tabNavigator = createBottomTabNavigator(
+    {
+        Test: {
+            screen: Test,
+            navigationOptions: {
+                tabBarLabel: "Test",
+                tabBarIcon: ({tintColor}) => (
+                    <Entypo name="edit" size={24} color={tintColor}/>
+                )
+            },
+        },
+        Camera: {
+            screen: Camera_v2,
+            navigationOptions: {
+                tabBarLabel: "Try",
+                tabBarIcon: ({tintColor}) => (
+                    <Entypo name="camera" size={24} color={tintColor}/>
+                )
+            },
+        },
+    },
+    {
+        tabBarOptions: {
+            showIcon: true,
+            labelStyle: {
+                fontSize: 15,
+            },
+            activeTintColor: 'blue',
+            inactiveTintColor: 'gray',
+            size: 40
+        }
+    }
+);
+
+const AppNav = createAppContainer(tabNavigator);
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <AppNav />
+        )
+    }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- components/screens/Camera_v2.js
+++ components/screens/Camera_v2.js
@@ -3,72 +3,111 @@ import {Text, View, Image, StyleSheet} from "react-native";
 import {Camera} from "expo-camera";
 import * as FaceDetector from "expo-face-detector";
 import * as Permissions from "expo-permissions";
-import {Icon, Button} from "native-base";
+import {NavigationEvents} from 'react-navigation';
 
 export default class CameraComponent extends Component {
+
+    cameraRef = React.createRef();
+
     state = {
-        faceSquare: {}
+        faceSquare: {},
+        loaded: true
     };
 
     async componentDidMount() {
         const {status} = await Permissions.askAsync(Permissions.CAMERA);
         this.setState({hasCameraPermission: status === "granted"});
+
     }
 
+    // Kode stykke, som burde håndtere at kamera bliver sort når man skifter via navigation, dog skal jeg finde en metode
+    // så jeg kan implementere det i en async componentDidMountMetode()
+
+    /*
+
+    componentDidMount() {
+        this.updateNavigation();
+    }
+
+    updateNavigation = async () => {
+
+        const {navigation} = this.props;
+        navigation.addListener('willFocus', () =>
+            this.setState({focusedScreen: true})
+        );
+        navigation.addListener('willBlur', () =>
+            this.setState({focusedScreen: false})
+        );
+    }
+
+     */
+
     render() {
+        const {loaded} = this.state;
         return (
             <View style={{width: "100%", flex: 1}}>
-                <Camera
-                    type={Camera.Constants.Type.front}
-                    style={{flex: 1, width: "100%"}}
-                    onFacesDetected={res => {
-                        console.log(res);
-                        if (res.faces[0]) {
-                            this.setState({
-                                faceSquare: {
-                                    width: res.faces[0].bounds.size.width,
-                                    height: res.faces[0].bounds.size.height,
-                                    marginLeft: res.faces[0].bounds.origin.x,
-                                    marginTop: res.faces[0].bounds.origin.y,
-                                    smillingProbability: res.faces[0].smilingProbability
+                <NavigationEvents
+                    onWillFocus={payload => this.setState({loaded: true})}
+                    onDidBlur={payload => this.setState({loaded: false})} />
+                    <View style={{width: "100%", flex: 1}}>
+                        {loaded && (
+
+
+                        <Camera
+                            type={Camera.Constants.Type.front}
+                            style={{flex: 1, width: "100%"}}
+                            onFacesDetected={res => {
+                                console.log(res);
+                                if (res.faces[0]) {
+                                    this.setState({
+                                        faceSquare: {
+                                            width: res.faces[0].bounds.size.width,
+                                            height: res.faces[0].bounds.size.height,
+                                            marginLeft: res.faces[0].bounds.origin.x,
+                                            marginTop: res.faces[0].bounds.origin.y,
+                                        }
+                                    });
                                 }
-                            });
-                        }
-                        if (res.faces.length == 0) {
-                            this.setState({
-                                faceSquare: {}
-                            });
-                        }
+                                if (res.faces.length == 0) {
+                                    this.setState({
+                                        faceSquare: {}
+                                    });
+                                }
 
-                    }}
-                    faceDetectorSettings={{
-                        mode: FaceDetector.Constants.Mode.fast,
-                        detectLandmarks: FaceDetector.Constants.Landmarks.none,
-                        runClassifications: FaceDetector.Constants.Classifications.all,
-                        minDetectionInterval: 100,
-                        tracking: true
-                    }}
-                >
-                    {Object.keys(this.state.faceSquare) ? (
-                        <View>
-                            <View
-                                style={{
 
-                                    width: this.state.faceSquare.width,
-                                    height: this.state.faceSquare.height,
-                                    marginLeft: this.state.faceSquare.marginLeft,
-                                    marginTop: this.state.faceSquare.marginTop,
-                                }}
+                            }}
+                            faceDetectorSettings={{
+                                mode: FaceDetector.Constants.Mode.fast,
+                                detectLandmarks: FaceDetector.Constants.Landmarks.none,
+                                runClassifications: FaceDetector.Constants.Classifications.all,
+                                minDetectionInterval: 100,
+                                tracking: true
+                            }}
+                        >
+                            {Object.keys(this.state.faceSquare) ? (
+                                <View>
+                                    <View
+                                        style={{
 
-                            >
+                                            width: this.state.faceSquare.width,
+                                            height: this.state.faceSquare.height,
+                                            marginLeft: this.state.faceSquare.marginLeft,
+                                            marginTop: this.state.faceSquare.marginTop,
+                                        }}
 
-                                <Image style={styles.logo}
-                                       source={{uri: "https://pics.clipartpng.com/White_Face_Mask_PNG_Clipart-3285.png"}}
-                                />
-                            </View>
-                        </View>
-                    ) : null}
-                </Camera>
+                                    >
+
+                                        <Image style={styles.logo}
+                                               source={{uri: "https://pics.clipartpng.com/White_Face_Mask_PNG_Clipart-3285.png"}}
+                                        />
+                                    </View>
+                                </View>
+                            ) : null}
+                        </Camera>
+                        )}
+
+                    </View>
+
             </View>
         );
     }
