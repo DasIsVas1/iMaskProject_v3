c364ebd656ab158ffc1f44d96a0d9243ca8f0038
dasisvas1 <krva18ab@student.cbs.dk>
2021-01-29 14:48:20 +0100

    Nu virker det hele!!! Dog kan der forekomme crashes, men genstart enten expo eller via terminal for at resolve

--- App.js
+++ App.js
@@ -5,19 +5,45 @@ import Camera_v2 from './components/screens/Camera_v2';
 import Test from "./components/screens/Test";
 import {createBottomTabNavigator} from "react-navigation-tabs";
 import {createAppContainer} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
 import {Entypo} from '@expo/vector-icons';
 import DesignMBScreen from "./components/screens/DesignMBScreen";
 import UserScreen from "./components/screens/UserScreen";
+import firebase from "firebase";
+import LoginScreen from "./components/LoginComponent/LoginScreen";
+import SignUpScreen from "./components/LoginComponent/SignUpScreen";
 
 
 
 
 // --------------------- FIREBASE IMPL. START ---------------------
 
+const fireBaseConfig = {
+    apiKey: "AIzaSyB2Xe40c4U_1MrC9ffo6ubNDHod5RLpih0",
+    authDomain: "innovationsprojektmbapp.firebaseapp.com",
+    databaseURL: "https://innovationsprojektmbapp.firebaseio.com",
+    projectId: "innovationsprojektmbapp",
+    storageBucket: "innovationsprojektmbapp.appspot.com",
+    messagingSenderId: "966473468096",
+    appId: "1:966473468096:web:7e9b4bd1f9ed33c8e7c525"
+};
+// vigtigt at tilføje nedestående if statement, da ellers init firebase flere gange
+if (!firebase.apps.length) {
+    firebase.initializeApp(fireBaseConfig);
+}
+
 // --------------------- FIREBASE IMPL. SLUT ---------------------
 
 // --------------------- LOGIN IMPL. START ---------------------
 
+const LoginNavigator = createStackNavigator(
+    {
+        Login: {screen: LoginScreen},
+        SignUp: {screen: SignUpScreen},
+    },
+);
+
+const LoginContainer = createAppContainer(LoginNavigator);
 
 // --------------------- LOGIN IMPL. SLUT ---------------------
 
@@ -75,7 +101,33 @@ const AppNav = createAppContainer(tabNavigator);
 
 // --------------------- BOTTOM NAVIGATOR SLUT ---------------------
 
+export default class App extends React.Component {
 
+    state = {user:null}
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({user});
+        });
+    }
+
+    render() {
+        const {user} = this.state
+
+        if (!user) {
+            return (
+                <LoginContainer />
+            )
+        } else {
+            return (
+                <AppNav user={user} />
+            )
+        }
+    }
+}
+
+
+/*
 export default class App extends React.Component {
     render() {
         return (
@@ -83,6 +135,8 @@ export default class App extends React.Component {
         )
     }
 }
+
+ */
 
 
 const styles = StyleSheet.create({
